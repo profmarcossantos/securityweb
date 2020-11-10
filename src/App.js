84f5644f6a5ac4003ab858273ae2a80005f06e63
Prof. Marcos R. Santos <marcos.santos@imed.edu.br>
2020-11-09 21:01:30 -0300

    09/11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import {
+  Button,
+  Grid,
+  Paper,
+  TextField,
+}
+  from '@material-ui/core';
+
+import Firebase from './services/FirebaseConnect'
 
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const login = () => {
+
+    Firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((retorno) => {
+        console.log("Usuário Logado: " + retorno.user.uid)
+      })
+      .catch((erro) => {
+        console.log(erro)
+      })
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container spacing={1}>
+        <Grid item sm={8} xs={12}>
+
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Paper elevation={0}>
+            <TextField
+              label="E-mail"
+              variant="outlined"
+              size="small"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ width: "100%", marginBottom: 10 }} />
+            <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label="Senha"
+              variant="outlined"
+              type="password"
+              size="small"
+              style={{ width: "100%", marginBottom: 10 }} />
+            <Button
+              onClick={login}
+              variant="outlined"
+              color="primary"
+              style={{ float: "right" }}>
+              Entrar
+            </Button>
+          </Paper>
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
---
+++ src/services/FirebaseConnect.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase'
+import config from './FirebaseConfig'
+
+firebase.initializeApp(config)
+
+export default firebase
